ccaaef616c96b80b23335ddd144bf46e82cc488c
Bhavay Kaushal <126071104+Bh4vay@users.noreply.github.com>
2025-03-20 20:51:52 +0530

    Delete 1/backend/models directory

--- 1/backend/models/Comment.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const commentSchema = mongoose.Schema({
-    postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
-    content: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Comment", commentSchema);
--- 1/backend/models/Post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const postSchema = mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    content: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Post', postSchema);
